a2148354314d2f200f9d603d20ec0456f516a6c8
theinhnam <nguyenduythanhnam24@gmail.com>
2024-05-28 16:13:58 +0700

    first commit

---
+++ src/test/java/com/namndt/demogit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namndt.demogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
